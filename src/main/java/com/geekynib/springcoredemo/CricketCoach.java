90bc242b7dc8e13909dc17be017f677b278e519e
nibchee <nirbhaybansal28@gmail.com>
2023-03-16 07:19:13 +0530

    Default Scanning of Packages

---
+++ src/main/java/com/geekynib/springcoredemo/CricketCoach.java
@@ -0,0 +1,15 @@
+package com.geekynib.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+
+//Component annotation marks the class as Spring Bean for Dependency Injection
+@Component
+public class CricketCoach implements Coach{
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Pratise Fast Bowling !!!";
+    }
+}

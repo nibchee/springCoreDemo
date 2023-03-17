d8082b6d048a6151546c20afa46d5f1af555dbb6
nibchee <nirbhaybansal28@gmail.com>
2023-03-18 00:49:34 +0530

    Setter INjection

---
+++ src/main/java/com/geekynib/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.geekynib.springcoredemo.rest;
+
+import com.geekynib.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define  a private field for the dependency
+    private Coach myCoach;
+
+    //define a construtor for dependecy injection
+//    @Autowired
+//    public DemoController(Coach theCoach){
+//        myCoach=theCoach;
+//    }
+
+
+    //Adding setter Injection
+    @Autowired
+    public void doSomeStuff(Coach theCoach){
+      myCoach=theCoach;
+    }
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
